b06f23fcd4c8c74345f9e1e74fe34b2205d220aa
Dmitriy Onypko <vaporif@gmail.com>
2024-11-07 14:23:39 +0000

    diagnostic symbols

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -96,11 +96,12 @@ return {
       end,
     })
 
-    local signs = { Error = '', Warn = '', Hint = '', Info = '' }
-    for type, icon in pairs(signs) do
-      local hl = 'DiagnosticSign' .. type
-      tsr
-      vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+    if vim.g.have_nerd_font then
+      local signs = { Error = '', Warn = '', Hint = '', Info = '' }
+      for type, icon in pairs(signs) do
+        local hl = 'DiagnosticSign' .. type
+        vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+      end
     end
 
     -- LSP servers and clients are able to communicate to each other what features they support.
