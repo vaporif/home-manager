06b6adbadafe7fed7622f0d2bd8a7233b07ee507
Dmytro Onypko <vaporif@gmail.com>
2024-11-23 22:18:19 +0000

    Add ci & format & drop reth (#1)
    * add ci
    
    * format
    
    * update name
    
    * drop reth

--- flake.nix
+++ flake.nix
@@ -3,7 +3,6 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    ethereum-nix.url = "github:nix-community/ethereum.nix";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -14,41 +13,41 @@
     };
   };
 
-  outputs = { nixpkgs, home-manager, ethereum-nix, fzf-git-sh, ... }: let
-    system = "aarch64-darwin";
-    pkgs = import nixpkgs { inherit system; };
-    fzf-git-sh-package = pkgs.writeShellScriptBin "fzf-git.sh" (builtins.readFile fzf-git-sh);
-    ethereum-nix-pkgs = ethereum-nix.packages.${system};
-  in {
-    homeConfigurations.vaporif = 
-      home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-        modules = [ 
-          {
-            home = {
-              username = "vaporif";
-              homeDirectory = "/Users/vaporif";
-              stateVersion = "24.05";
-            };
-          }
-          ./home.nix  
-          # I don't want to use nix-darwin as support is brittle
-          # easier to install alacritty + karabiner via brew & hook up configs
-          {
-            home.file = {
-              ".config/alacritty".source = ./alacritty;
-              ".config/karabiner".source = ./karabiner;
-            };
-          }
-          {
-            xdg.configFile."zellij/config.kdl".text = "${builtins.readFile ./zellij/config.kdl}";
-          }
-        ];
-        extraSpecialArgs = {
-          inherit fzf-git-sh-package;
-          inherit ethereum-nix-pkgs;
+  outputs = { nixpkgs, home-manager, fzf-git-sh, ... }:
+    let
+      system = "aarch64-darwin";
+      pkgs = import nixpkgs { inherit system; };
+      fzf-git-sh-package = pkgs.writeShellScriptBin "fzf-git.sh" (builtins.readFile fzf-git-sh);
+    in
+    {
+      homeConfigurations.vaporif =
+        home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          modules = [
+            {
+              home = {
+                username = "vaporif";
+                homeDirectory = "/Users/vaporif";
+                stateVersion = "24.05";
+              };
+            }
+            ./home.nix
+            # I don't want to use nix-darwin as support is brittle
+            # easier to install alacritty + karabiner via brew & hook up configs
+            {
+              home.file = {
+                ".config/alacritty".source = ./alacritty;
+                ".config/karabiner".source = ./karabiner;
+              };
+            }
+            {
+              xdg.configFile."zellij/config.kdl".text = "${builtins.readFile ./zellij/config.kdl}";
+            }
+          ];
+          extraSpecialArgs = {
+            inherit fzf-git-sh-package;
+          };
         };
-      };
     };
 }
 
