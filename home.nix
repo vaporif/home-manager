8b72e3aa2ab75c5b2c0b53187af8b273f729bce3
Dmytro Onypko <vaporif@gmail.com>
2024-11-25 03:20:47 +0000

    add lf icons

--- home.nix
+++ home.nix
@@ -108,4 +108,8 @@
   };
 
   xdg.configFile.nvim.source = ./nvim;
+  xdg.configFile."lf/icons".source = builtins.fetchurl {
+    url = "https://raw.githubusercontent.com/gokcehan/lf/master/etc/icons.example";
+    sha256 = "12cwy6kfa2wj7nzffaxn5bka21yjqa5sx38nzdhyg1dq0c6jnjkk";
+  };
 }
