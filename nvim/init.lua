0c926ea3dc8abee0210ddfb2a035d446ffef8086
Dmytro Onypko <vaporif@gmail.com>
2024-12-13 20:18:31 +0000

    disable cecutil (#11)

--- nvim/init.lua
+++ nvim/init.lua
@@ -18,6 +18,7 @@ vim.opt.list = true
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 vim.opt.inccommand = 'split'
 vim.opt.cursorline = true
+vim.g.no_cecutil_maps = 1
 
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 15
