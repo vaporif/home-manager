d2e2eb2b463d680b68a43dda2c597faa7123a219
Dmytro Onypko <vaporif@gmail.com>
2025-03-15 12:18:11 +0000

    upd

--- home.nix
+++ home.nix
@@ -139,6 +139,4 @@
     url = "https://raw.githubusercontent.com/gokcehan/lf/master/etc/icons.example";
     sha256 = "12cwy6kfa2wj7nzffaxn5bka21yjqa5sx38nzdhyg1dq0c6jnjkk";
   };
-
-  # xdg.configFile."direnv/direnvrc"
 }
