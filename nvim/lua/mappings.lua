a17766eb777882c3fe4eb39ce615c70ab94d17a2
Dmytro Onypko <vaporif@gmail.com>
2025-03-25 16:35:13 +0000

    mapping updae

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -11,6 +11,7 @@ vim.keymap.set('n', '<leader>cd', '<cmd>RustLsp debuggables<Cr>', { desc = '[D]e
 vim.keymap.set('n', '<leader>cd', '<cmd>RustLsp logFile<Cr>', { desc = '[L]og file' })
 vim.keymap.set('n', '<leader>cl', '<cmd>DiffviewOpen<Cr>', { desc = 'diff too[l]' })
 vim.keymap.set('n', '<leader>ci', '<cmd>AnsiEsc<Cr>', { desc = 'Ans[i] escape' })
+vim.keymap.set('n', '<leader>ck', vim.diagnostic.setloclist, { desc = 'Quic[k]fix list' })
 
 vim.keymap.set('n', '<leader>/', 'gcc', { desc = 'Toggle Comment', remap = true })
 vim.keymap.set('v', '<leader>/', 'gc', { desc = 'Toggle comment', remap = true })
@@ -21,17 +22,17 @@ vim.keymap.set('n', '<Leader>a', '<Cmd>Neotree float git_status toggle reveal<CR
 
 -- Buffers
 vim.keymap.set('n', '<leader>bn', ':bnext<CR>', { noremap = true, desc = '[N]ext buffer' })
+vim.keymap.set('n', '<leader>bw', ':bnext<CR>', { noremap = true, desc = '[N]ext buffer' })
 vim.keymap.set('n', '<leader>bp', ':bprevious<CR>', { noremap = true, desc = '[P]revious buffer' })
-vim.keymap.set('n', '<leader>bb', '<C-^>', { noremap = true, desc = 'Toggle previous [b]uffer' })
-vim.keymap.set('n', '<leader>bd', ':bdelete<CR>', { noremap = true, desc = 'Close current' })
-vim.keymap.set('n', '<leader>bo', ':%bd|e#|bd#<CR>', { noremap = true, desc = 'Close except current' })
+vim.keymap.set('n', '<leader>bb', '<C-^>', { noremap = true, desc = 'Toggle [b]uffer' })
+vim.keymap.set('n', '<leader>bd', ':bdelete<CR>', { noremap = true, desc = '[D]elete current' })
+vim.keymap.set('n', '<leader>bo', ':%bd|e#|bd#<CR>', { noremap = true, desc = 'Cl[o]se except current' })
 vim.keymap.set('n', '<leader>st', '<C-w>w', { noremap = true, desc = '[T]oggle splits' })
 vim.keymap.set('n', '<leader>sv', ':vsplit<CR>', { noremap = true, desc = 'Split [v]ertically' })
 vim.keymap.set('n', '<leader>sh', ':split<CR>', { noremap = true, desc = 'Split [h]orizontally' })
-vim.keymap.set('n', '<leader>m', '<C-w>w', { noremap = true, desc = 'Split toggle' })
--- vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = '[Q]uickfix list' })
+vim.keymap.set('n', '<leader>l', '<C-w>w', { noremap = true, desc = 'Split togg[l]e' })
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
-vim.keymap.set('n', '<leader>s', ':SearchAndSub<CR>', { noremap = true, silent = true, desc = '[M]ulti replace' })
+vim.keymap.set('n', '<leader>m', ':SearchAndSub<CR>', { noremap = true, silent = true, desc = '[M]ulti replace' })
 vim.keymap.set('n', '<leader>co', function()
   for _, client in pairs(vim.lsp.get_clients()) do
     vim.lsp.stop_client(client.id)
