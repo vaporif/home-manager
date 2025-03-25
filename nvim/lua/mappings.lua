49dcf5437dd3d5c8ac2e03f8d3deaba6750a9658
Dmytro Onypko <vaporif@gmail.com>
2025-03-25 17:04:16 +0000

    upd

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -28,7 +28,7 @@ vim.keymap.set('n', '<leader>bo', ':%bd|e#|bd#<CR>', { noremap = true, desc = 'c
 vim.keymap.set('n', '<leader>st', '<C-w>w', { noremap = true, desc = '[t]oggle' })
 vim.keymap.set('n', '<leader>sv', ':vsplit<CR>', { noremap = true, desc = '[v]ertically' })
 vim.keymap.set('n', '<leader>sh', ':split<CR>', { noremap = true, desc = '[l]orizontally' })
-vim.keymap.set('n', '<leader>l', '<C-w>w', { noremap = true, desc = 'togg[l]e' })
+vim.keymap.set('n', '<leader>l', '<C-w>w', { noremap = true, desc = 'buffer togg[l]e' })
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 vim.keymap.set('n', '<leader>m', ':SearchAndSub<CR>', { noremap = true, silent = true, desc = '[m]ulti replace' })
 vim.keymap.set('n', '<leader>co', function()
