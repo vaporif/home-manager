bf6cdbec42d3677b944c5fb22b772f314fa5ed85
Dmytro Onypko <vaporif@gmail.com>
2025-04-01 16:27:48 +0100

    upd

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -49,7 +49,7 @@ return {
 
         -- Execute a code action, usually your cursor needs to be on top of an error
         -- or a suggestion from your LSP for this to activate.
-        map('<leader>ca', vim.lsp.buf.code_action, '[c]ode [a]ction')
+        map('<leader>ca', vim.lsp.buf.code_action, 'code [a]ction')
 
         -- WARN: This is not Goto Definition, this is Goto Declaration.
         --  For example, in C this would take you to the header.
